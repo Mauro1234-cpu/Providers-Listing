387c0b1ebf7660c51f65ab38c46dda37fd0d362b
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-09 09:51:48 -0300

    style: improve login page

--- src/routes/(public)/_guest/login/page.tsx
+++ src/routes/(public)/_guest/login/page.tsx
@@ -1,11 +1,16 @@
+import { useTranslation } from "react-i18next";
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Header } from "../header";
 import { LoginForm } from "./-components";
 
 const LoginPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <div className="w-96 rounded-2xl p-5 shadow">
+      <div className="w-80 rounded-2xl border border-border-default-default p-5">
+        <Header description={t("login.description")} title={t("login.welcome")} />
         <LoginForm />
       </div>
     </div>
