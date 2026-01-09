bb5266c37face2811cabb068528bff264bea75ca
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-09 09:58:08 -0300

    feat: create service response

--- src/services/auth/types.ts
+++ src/services/auth/types.ts
@@ -2,5 +2,9 @@ import type { z } from "zod";
 
 import type { getLoginPayloadSchema, loginResponseSchema } from "./schemas";
 
+export type ServiceResponse<T> = {
+  data: T;
+};
+
 export type LoginPayload = z.infer<ReturnType<typeof getLoginPayloadSchema>>;
 export type LoginResponse = z.infer<typeof loginResponseSchema>;
