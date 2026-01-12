915b8139c28e6dcae812d80c2faed4f0d5327f7a
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-12 10:52:05 -0300

    feat: create action and route

--- src/services/auth/api.ts
+++ src/services/auth/api.ts
@@ -1,6 +1,21 @@
 import { publicApi } from "@/config/api";
-import type { LoginPayload, LoginResponse, ServiceResponse } from "./types";
+import type {
+  LoginPayload,
+  LoginResponse,
+  RegisterPayload,
+  RegisterResponse,
+  ServiceResponse,
+} from "./types";
 
 export const login = ({ email, password }: LoginPayload) => {
   return publicApi.post<ServiceResponse<LoginResponse>>("auth/login", { email, password });
+};
+
+export const register = ({ email, name, password, passwordConfirm }: RegisterPayload) => {
+  return publicApi.post<ServiceResponse<RegisterResponse>>("auth/register", {
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
 };
