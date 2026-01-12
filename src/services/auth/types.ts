0bfe0a9fc58728e14c3b71dc01f323ed5a834ec1
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-12 10:51:20 -0300

    feat: create register payload and response

--- src/services/auth/types.ts
+++ src/services/auth/types.ts
@@ -1,6 +1,11 @@
 import type { z } from "zod";
 
-import type { getLoginPayloadSchema, loginResponseSchema } from "./schemas";
+import type {
+  getLoginPayloadSchema,
+  getRegisterPayloadSchema,
+  loginResponseSchema,
+  registerResponseSchema,
+} from "./schemas";
 
 export type ServiceResponse<T> = {
   data: T;
@@ -8,3 +13,6 @@ export type ServiceResponse<T> = {
 
 export type LoginPayload = z.infer<ReturnType<typeof getLoginPayloadSchema>>;
 export type LoginResponse = z.infer<typeof loginResponseSchema>;
+
+export type RegisterPayload = z.infer<ReturnType<typeof getRegisterPayloadSchema>>;
+export type RegisterResponse = z.infer<typeof registerResponseSchema>;
