7063c10aeb991980be03fd16f182053ca7382a06
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-13 12:00:33 -0300

    style: correct button style

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -8,7 +8,7 @@ const inputVariants = tv({
   slots: {
     container: "relative flex w-full flex-col gap-1.5",
     input:
-      "flex w-full rounded-md border border-border-default-default px-3 py-1 text-base text-text-default-default transition-colors placeholder:text-text-disabled-default focus:border-border-brand-default focus:outline-none focus-visible:outline-border-brand-default disabled:cursor-not-allowed disabled:bg-background-disabled-default disabled:text-text-disabled-on-disabled md:text-sm",
+      "flex w-full rounded-md border border-border-default-default bg-background-base-default px-3 py-1 text-base text-text-default-default transition-colors placeholder:text-text-disabled-default focus:border-border-brand-default focus:outline-none focus-visible:outline-border-brand-default disabled:cursor-not-allowed disabled:bg-background-disabled-default disabled:text-text-disabled-on-disabled md:text-sm",
     wrapper: "relative flex flex-row items-center rounded-md",
     leftIcon:
       "pointer-events-none absolute top-1/2 left-2 flex -translate-y-1/2 items-center text-text-default-default",
