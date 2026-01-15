3e5627432c1f672841b24bee91ac3c8ac90fb828
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-15 13:38:20 -0300

    feat: add succes animation

--- src/routes/(public)/_guest/success/-components/success.tsx
+++ src/routes/(public)/_guest/success/-components/success.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { useNavigate } from "@tanstack/react-router";
 
 import { Button } from "@/components";
+import { SuccessAnimation } from "./success-animation";
 
 export const Success = () => {
   const { t } = useTranslation();
@@ -9,16 +10,17 @@ export const Success = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="dm-sans relative flex h-full min-h-screen flex-col items-center justify-center bg-linear-to-t from-emerald-300/60 to-emerald-100/20 bg-center bg-no-repeat px-4">
+    <div className="dm-sans relative flex h-full min-h-screen flex-col items-center justify-center gap-30 bg-linear-to-t from-background-success-secondary-hover/60 to-background-success-secondary/20 bg-center bg-no-repeat px-4">
       <div className="flex w-full flex-col items-center px-5">
-        <div className="my-30 flex flex-col items-center gap-5 text-center md:w-4/5">
-          {/* animation icon */}
+        <div className="flex flex-col items-center text-center md:w-4/5">
+          <SuccessAnimation />
           <h2 className="text-3xl">{t("register.success")}</h2>
           <p className="text-base font-normal">{t("register.successDesc")}</p>
         </div>
       </div>
 
       <Button
+        className="w-103"
         onClick={() => {
           navigate({ to: "/login" });
         }}
