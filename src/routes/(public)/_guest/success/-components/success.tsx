6cef6db10704912ae3735e71fee749ba322e0da3
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-16 09:34:21 -0300

    style: correct button position on desktop views

--- src/routes/(public)/_guest/success/-components/success.tsx
+++ src/routes/(public)/_guest/success/-components/success.tsx
@@ -11,7 +11,7 @@ export const Success = () => {
 
   return (
     <div className="dm-sans relative flex h-full min-h-screen flex-col items-center justify-center gap-30 bg-linear-to-t from-background-success-secondary-hover/60 to-background-success-secondary/20 bg-center bg-no-repeat px-4">
-      <div className="flex w-full flex-1 flex-col items-center justify-center px-5">
+      <div className="flex w-full flex-1 flex-col items-center justify-center px-5 md:flex-none">
         <div className="flex flex-col items-center text-center md:w-4/5">
           <SuccessAnimation />
           <h2 className="text-3xl">{t("register.success")}</h2>
