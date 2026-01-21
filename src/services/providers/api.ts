a8a69eb668cafa46613aaa2f7f93b772175b23ba
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-21 10:00:55 -0300

    fix: incorrect response schema

--- src/services/providers/api.ts
+++ src/services/providers/api.ts
@@ -1,11 +1,11 @@
 import z from "zod";
 
 import { privateApi } from "@/config/api";
-import { getProviderSchema } from "./schemas";
+import { paginatedResponse } from "./schemas";
 import type { ProviderRequestParams } from "./types";
 
 export const getProviders = async (params: ProviderRequestParams) => {
   const data = await privateApi.get("providers", { params });
 
-  return (z.array(getProviderSchema()), data.data);
+  return (z.array(paginatedResponse()), data.data);
 };
