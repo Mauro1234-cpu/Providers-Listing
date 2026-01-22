37189e8d7deef241aa3e48c913aae7ec41dd1f56
Mauro Aires <mauroaires@MacBook-Air-de-Mauro.local>
2026-01-22 13:53:46 -0300

    feat: use the profile pic of the user as a log out button

--- src/routes/_private/-components/header.tsx
+++ src/routes/_private/-components/header.tsx
@@ -7,15 +7,8 @@ type HeaderProps = {
   text: string;
 };
 
-export const Header = ({ desc, text, title }: HeaderProps) => {
+export const Header = ({ desc, title }: HeaderProps) => {
   return (
-    // <header className="flex items-center justify-between bg-background-brand-default p-4 text-text-brand-on-brand">
-    //   <img className="h-10" src="./logo.svg" />
-
-    //   <span>{t("greetings.rootLayout")}</span>
-
-    //
-    // </header>
     <div className="border-border-primary mx-5 flex flex-row items-center justify-between border-b py-5 lg:mx-0 lg:px-35">
       <div className="flex flex-row items-center">
         <Logo className="w-11" />
@@ -23,11 +16,8 @@ export const Header = ({ desc, text, title }: HeaderProps) => {
           <h1 className="text-primary text-2xl font-semibold">{title}</h1>
           <p className="text-tertiary text-sm">{desc}</p>
         </div>
-        <HamburgerMenu />
       </div>
-      <p className="bg-default flex size-10 items-center justify-center rounded-full leading-5 text-white">
-        {text}
-      </p>
+      <HamburgerMenu text={initialsName || ""} />
     </div>
   );
 };
